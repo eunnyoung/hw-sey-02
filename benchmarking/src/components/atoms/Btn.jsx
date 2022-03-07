2faa63961a180544a23817c0a5ecd5e5a8f3f7a0
SEO EUNYOUNG <eyoung6u6@daum.net>
2022-03-07 15:02:07 +0900

    BtnBorder 비구조화

--- benchmarking/src/components/atoms/Btn.jsx
+++ benchmarking/src/components/atoms/Btn.jsx
@@ -1,23 +1,13 @@
 import styled, { css } from 'styled-components';
 
 const BtnBorder = styled.div`
-color: ${(color) => color};
+  border-color: ${({borderColor}) => borderColor};
+    color: ${({color}) => color};
     padding: 10px;
     padding-inline: 30px;
     border: solid;
     border-radius: 5px;
 `;
-
-// const BtnBorder = styled.div`
-//     border-color: ${(props) => props.borderColor};
-//     color: ${(props) => props.color};
-//     border-color: ${(borderColor) => borderColor};
-//     color: ${(color) => color};
-//     padding: 10px;
-//     padding-inline: 30px;
-//     border: solid;
-//     border-radius: 5px;
-// `;
 
 export default function Btn ( { children, borderColor, color} ) {
     return <BtnBorder border-color={borderColor} color={color}>{children}</BtnBorder>;
